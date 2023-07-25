bcbc056b08d4d1204c425e637e4c41ec750dda94
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-07-26 00:49:49 +0500

    Added mobile side bar using sheet

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
